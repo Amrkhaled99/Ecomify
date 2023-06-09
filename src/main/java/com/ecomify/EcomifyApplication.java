9a0e7390c8dd749caa51004f73b046031698d4b4
amrkhaled99 <amrkh5255@gmail.com>
2023-06-10 00:41:46 +0300

    basic project structure

---
+++ src/main/java/com/ecomify/EcomifyApplication.java
@@ -0,0 +1,13 @@
+package com.ecomify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecomify/EcomifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
